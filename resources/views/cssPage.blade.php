bb2aa44a2a2d4404974d37269771722055b1f5cb
alexandria456 <alexandriakiassa@gmail.com>
2022-11-22 22:53:41 +0300

    -css

---
+++ resources/views/cssPage.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<ul>
+    <li><a href="">Пункт меню</a></li>
+    <li><a href="">Пункт меню</a></li>
+    <li><a href="">Пункт меню</a></li>
+</ul>
+<div>
+    <img alt="img" src="storage/app/public/image.jpg">
+</div>
+
+</body>
+</html>
